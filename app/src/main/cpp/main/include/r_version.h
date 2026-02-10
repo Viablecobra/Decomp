aca07578429caefba193f0cd3fc3ec9cd45b10f9
vcx <ryansk0987@gmail.com>
2026-02-10 17:30:07 +0530

    update r_version.h

---
+++ app/src/main/cpp/main/include/r_version.h
@@ -0,0 +1,13 @@
+#ifndef R_VERSION_H
+#define R_VERSION_H 1
+#define R_MESON_VERSION "1.0.0"
+#define R2_VERSION_MAJOR 6
+#define R2_VERSION_MINOR 0
+#define R2_VERSION_PATCH 8
+#define R2_VERSION_NUMBER 60008
+#define R2_VERSION_COMMIT 0
+#define R2_VERSION "6.0.8"
+#define R2_GITTAP "6.0.8"
+#define R2_GITTIP "android"
+#define R2_BIRTH "2024-01-01"
+#endif
